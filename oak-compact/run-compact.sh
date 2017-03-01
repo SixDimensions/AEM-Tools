8c83a1c9ff80e9a6b7ad62d5400b40a6448a685a
Dwayne Hale <dwayne.hale@gmail.com>
2017-03-01 16:28:41 -0500

    Updated with some things. Primarily the ability to stop and start AEM for compaction.

--- oak-compact/run-compact.sh
+++ oak-compact/run-compact.sh
@@ -6,23 +6,41 @@ function usage {
   echo "-v       The version of oak-compact.jar to use."
   echo "-m       The mode of cleanup, this can currently be rm-unreferenced or rm-all"
   echo "-b       Set to true to backup the repository before compacting, backup will occur in same directory as repo."
-  echo "-s       Specify Xms memory requirement for JVM."
-  echo "-x       Specify Xmx memory requirement for JVM."
+  echo "-j       Specify additional JVM options for oak run jar (such as memory requirements)"
+  echo "-p       Specify the crx-quickstart location for locating the pid file."
   echo -e "-H       displays this help and then exits"\\n
   echo -e "Example: run-compact.sh -d /opt/aem/crx-quickstart/repository/segmentstore -v 1.0.8 -m rm-all"
   exit
 }
+# configurations
+START='/etc/init.d/cq-publish start'
+STOP='/etc/init.d/cq-publish stop'
+OAK_JARS_LOCATION="/local/cq"
+COMPACTION_USER="cq"
+#START="service aem-author-6-2 start"
+#STOP="service aem-author-6-2 stop"
+SLEEPTIME='120s'
 
+
+function stopAEM {
+  PID=`cat $1/conf/cq.pid`
+  echo `date +"%h %d %Y %r"`" [INFO] Attempting to stop AEM with PID of $PID"
+  $STOP
+  while [ -n "`ps aux | grep 'java' | head -1 | grep ${PID}`" ]; do
+    echo `date +"%h %d %Y %r"`" [INFO] Instance not stopped completely, sleeping for $SLEEPTIME"
+    sleep $SLEEPTIME
+  done
+}
 function compact {
-  if [ aem_not_running ]; then
-    if [ ! -f oak_run_jars/oak-run-${1}.jar ];
+
+    if [ ! -f ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${1}.jar ];
       then
-       echo "Could not find valid oak run jar in the oak_run_jars directory."
+       echo `date +"%h %d %Y %r"`" [INFO] Could not find valid oak run jar in the oak_run_jars directory."
        exit 1
     fi
     if [ ! -d "$2" ];
       then
-        echo "Could not find directory for the repository."
+        echo `date +"%h %d %Y %r"`" [INFO] Could not find directory for the repository."
         exit 1
       fi
       if [ -z "$3" ];
@@ -37,40 +55,40 @@ function compact {
         then
         if [ "$4" = 'true' ];
           then
-            java -jar oak_run_jars/oak-run-$VERSION.jar backup "${repoDir}" ${repoDir}_bak
+            java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-$VERSION.jar backup "${repoDir}" ${repoDir}_bak
         fi
       fi
-      ## check if they supplied memory args
-      if [ -n "$5" ];
+      if [ -n "$pidFile" ];
         then
-          XMX="-Xmx$5"
+          stopAEM $pidFile
       fi
-      if [ -n "$6" ];
-        then
-         XMS="-Xms$6"
-      fi 
+
       VERSION=$1
       repoDir=$2
+      echo "Switching user to ${COMPACTION_USER}"
+
       # check for non-needed checkpoints
-      echo -e "Using oak-run-${VERSION}.jar...\n"
-      echo -e "Checking for checkpoints at $repoDir...\n"
-      echo "java $XMX $XMS -jar oak_run_jars/oak-run-${VERSION}.jar checkpoints ${repoDir}"
-      java $XMX $XMS -jar oak_run_jars/oak-run-${VERSION}.jar checkpoints "${repoDir}"
+      echo -e `date +"%h %d %Y %r"`" [INFO] Using oak-run-${VERSION}.jar...\n"
+      echo -e `date +"%h %d %Y %r"`" [INFO] Checking for checkpoints at $repoDir...\n"
+      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar checkpoints "${repoDir}"" - ${COMPACTION_USER}
       # rm the checkpoints
-      echo -e "Removing unreferenced checkpoints at $repoDir...\n"
-      java $XMX $XMS -jar oak_run_jars/oak-run-${VERSION}.jar checkpoints "${repoDir}" ${MODE}
+      echo -e `date +"%h %d %Y %r"`" [INFO] ${MODE} checkpoints at $repoDir...\n"
+      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar checkpoints "${repoDir}" ${MODE}" - ${COMPACTION_USER}
       #compact
-      echo -e "Compacting segmentstore at $repoDir...\n"
-      java $XMX $XMS -jar oak_run_jars/oak-run-${VERSION}.jar compact "${repoDir}"
-      echo -e "Done!\n"
+      echo -e `date +"%h %d %Y %r"`" [INFO] Compacting segmentstore at $repoDir...\n"
+      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar compact "${repoDir}"" - ${COMPACTION_USER}
+      echo -e `date +"%h %d %Y %r"`" [INFO] Done!\n"
+      if [ -n "$pidFile" ];
+        then
+         echo "Starting AEM..."
+         $START
+      fi
       exit 0
-    fi
 }
 # function that gets called to getopts and check them.
 function init {
-  logDate=`date +"%h %d %Y %r"`
-  echo -e "${logDate} [INFO] Starting offline oak compaction..."
-  while getopts ":d:H:v:m:b:p:x:s:" arg; do
+  echo -e `date +"%h %d %Y %r"`" [INFO] Starting offline oak compaction..."
+  while getopts ":d:H:v:m:b:p:j:" arg; do
     case "${arg}" in
       d)
           repoDir=$OPTARG
@@ -90,12 +108,9 @@ function init {
       p)
           pidFile=$OPTARG
           ;;
-      s)
-          xms=$OPTARG
+      j)
+          javaOpts=$OPTARG
           ;;
-      x)
-          xmx=$OPTARG
-          ;;              
      *)
          usage
         ;;
@@ -106,9 +121,9 @@ function init {
     echo "It appears you did not specify all required arguments!"
     usage
   fi
-  echo -e "${logDate} [INFO] Compacting ${repoDir} with jar version ${version}..."
+  echo -e `date +"%h %d %Y %r"`" [INFO] Compacting ${repoDir} with jar version ${version}..."
 
- compact $version $repoDir $mode $backup $xmx $xms
+ compact $version $repoDir $mode $backup $javaOpts
 
  exit 0
 }
