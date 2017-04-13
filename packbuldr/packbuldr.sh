856c4ca595026d92d3105eba00961c79b0acc9d9
Dwayne Hale <dwayne.hale@gmail.com>
2017-04-13 15:56:54 -0400

    Added some additional options, fixed the download issue.

--- packbuldr/packbuldr.sh
+++ packbuldr/packbuldr.sh
@@ -3,10 +3,8 @@
 
 #config
 HOST=""
-PORT="4502"
 # only configure if you plan to migrate content between two instances.
 TARGET_HOST=""
-TARGET_PORT=""
 GROUP_NAME=""
 PACKAGE_NAME=""
 USER="admin"
@@ -17,21 +15,21 @@ function usage {
   echo "-p       Password for configured user."
   echo "-n       The package name to use."
   echo "-f       Filter file to use."
-  echo "-m       If set to 'true', script will upload built package to another AEM instance and install."
+  echo "-t       If set to a host string such as http://localhost:4502, it will upload and install package on that host."
+  echo "-h       Use to specify host name of machine to build package from."
   echo -e "-H       displays this help and then exits"\\n
-  echo -e "Example: packbuldr.sh -p myuserpassword -n my_new_package"
+  echo -e "Example: packbuldr.sh -p myuserpassword -n my_new_package -h http://localhost:4502"
   exit
 }
 
 function createPackage {
   # create package
   #echo "creating package ${PACKAGE_NAME}..."
-  curl -u ${USER}:${PASS} -X POST ${HOST}:${PORT}/crx/packmgr/service/.json/etc/packages/${GROUP_NAME}/${PACKAGE_NAME}?cmd=create -d packageName=${PACKAGE_NAME} -d groupName=${GROUP_NAME}
+  curl -u ${USER}:${PASS} -X POST ${HOST}/crx/packmgr/service/.json/etc/packages/${GROUP_NAME}/${PACKAGE_NAME}?cmd=create -d packageName=${PACKAGE_NAME} -d groupName=${GROUP_NAME}
 }
 
 function addPackageFilters {
   # add filters
-  #echo "adding filters..."
   echo -e "\n"
   ROOTPATH=
   while IFS= read -r LINE
@@ -43,31 +41,26 @@ function addPackageFilters {
             ROOTPATH=${ROOTPATH},${path}
           fi
   done < "${FILTERS}"
-  curl -u ${USER}:${PASS} -X POST ${HOST}:${PORT}/crx/packmgr/update.jsp -F path=/etc/packages/${GROUP_NAME}/${PACKAGE_NAME}.zip -F packageName=${PACKAGE_NAME} -F groupName=${GROUP_NAME} -F 'filter=['$ROOTPATH']' -F '_charset_=UTF-8'
+  curl -u ${USER}:${PASS} -X POST ${HOST}/crx/packmgr/update.jsp -F path=/etc/packages/${GROUP_NAME}/${PACKAGE_NAME}.zip -F packageName=${PACKAGE_NAME} -F groupName=${GROUP_NAME} -F 'filter=['$ROOTPATH']' -F '_charset_=UTF-8'
 }
 # build package
 function buildPackage {
   #echo "building package ${PACKAGE_NAME}... "
   echo -e "\n"
-  curl -u ${USER}:${PASS} -X POST ${HOST}:${PORT}/crx/packmgr/service/exec.json/etc/packages/${GROUP_NAME}/${PACKAGE_NAME}.zip?cmd=build
+  curl -u ${USER}:${PASS} -X POST ${HOST}/crx/packmgr/service/exec.json/etc/packages/${GROUP_NAME}/${PACKAGE_NAME}.zip?cmd=build
 }
 function downloadPackage {
   echo -e "\n"
-  curl -u ${USER}:${PASS} ${HOST}:${PORT}/etc/packages/${GROUP_NAME}/${PACKAGE_NAME}.zip > ${PACKAGE_NAME}.zip
+  curl -u ${USER}:${PASS} -X POST -F path="/etc/packages/${GROUP_NAME}/${PACKAGE_NAME}.zip" -F _charset_="=utf-8" ${HOST}/crx/packmgr/download.jsp > ${PACKAGE_NAME}.zip
 }
 function uploadPackage {
   echo -e "\n"
-  curl -u ${USER}:${PASS} -F file=@"${PACKAGE_NAME}.zip" -F name="${PACKAGE_NAME}" -F force=true -F install=true ${TARGET_HOST}:${TARGET_PORT}/crx/packmgr/service.jsp
+  curl -u ${USER}:${PASS} -F file=@"${PACKAGE_NAME}.zip" -F name="${PACKAGE_NAME}" -F force=true -F install=true ${TARGET_HOST}/crx/packmgr/service.jsp
 }
-# function that gets called to getopts and check them.
-function init {
-  while getopts ":p:n:f:m:H:" arg; do
+while getopts ":p:n:f:m:h:t:H:" arg; do
     case "${arg}" in
       p)
           PASS=$OPTARG
-          ;;
-      H)
-          usage
           ;;
       n)
           PACKAGE_NAME=$OPTARG
@@ -77,6 +70,15 @@ function init {
           ;;
       m)
           MIGRATE=$OPTARG
+          ;;
+      h)
+          HOST=$OPTARG
+          ;;
+      t)
+          TARGET_HOST=$OPTARG
+          ;;
+      H)
+          usage
           ;;
       *)
          usage
@@ -92,9 +94,7 @@ function init {
   addPackageFilters
   buildPackage
   downloadPackage
-  if [ "${MIGRATE}" == "true" ]; then
+  if [ -n "${TARGET_HOST}" ]; then
     uploadPackage
   fi
   exit 0
-}
-init $@ #call to init function
