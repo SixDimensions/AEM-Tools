083cdbe33d316b00d5549ef90cdd65dbecdbba4b
Dwayne Hale <dwayne.hale@gmail.com>
2017-06-07 18:17:57 -0400

    normalized all the variables to uppercase, cleaned up some stuff.

--- oak-compact/run-compact.sh
+++ oak-compact/run-compact.sh
@@ -18,30 +18,31 @@ function usage {
 START='/etc/init.d/aem-author start'
 STOP='/etc/init.d/aem-author stop'
 ITYPE='' #blank for author, this is for internal error reporting.
-OAK_JARS_LOCATION="/home/aem"
-COMPACTION_USER="aem"
-SLEEPTIME='120s'
+OAK_JARS_LOCATION="/opt/oak-compact"
+COMPACTION_USER="cq"
+SLEEPTIME='20s'
 TIMEOUT=5
 # email notification configuration
-HOST=""
-TO="myemail@email.com"
+HOST="HOSTNAME"
+TO="youremail@email.com"
 
 #functions
 function sendAlertEmail {
-  echo "Subject: Offline Compaction failed on ${HOST}\nOffline compaction failed on ${HOST} for reason: ${1}\n" | /usr/sbin/sendmail ${TO}
+	echo -e "$1"
+  echo -e "Subject: Offline Compaction failed on ${HOST}\nOffline compaction failed on ${HOST} for reason: ${1}\n" | /usr/sbin/sendmail ${TO}
 }
 function validateAEMStart {
     COUNT=1
-    PID=`cat $1/conf/cq.pid`
-    echo `date +"%h %d %Y %r"`" [INFO] Attempting to start AEM with PID of $PID"
-    while [ -z "`ps aux | grep 'java' | grep ${PID}`" ]; do
+    PID=`cat ${PID_FILE}/conf/cq.pid`
+    echo `date +"%h %d %Y %r"`" [INFO] Attempting to start AEM with PID of ${PID}"
+    while [ -z "`ps aux | grep 'java' | head -n -1 | grep ${PID}`" ]; do
       if [ ${COUNT} -ge ${TIMEOUT} ]; then
         echo `date +"%h %d %Y %r"`" [WARN] Instance failed to start, could not find PID of ${PID}."
         REASON="${ITYPE}Instance failed to start, could not find PID of ${PID}."
-        #sendAlertEmail "${REASON}"
+        sendAlertEmail "${REASON}"
         exit 1
       fi
-      echo `date +"%h %d %Y %r"`" [INFO] Instance not started completely, sleeping for $SLEEPTIME, check number ${COUNT}"
+      echo `date +"%h %d %Y %r"`" [INFO] Instance not started completely, sleeping for ${SLEEPTIME}, check number ${COUNT}"
       sleep ${SLEEPTIME}
       ((COUNT++))
     done
@@ -49,14 +50,14 @@ function validateAEMStart {
 }
 function stopAEM {
   COUNT=1
-  PID=`cat $1/conf/cq.pid`
-  echo `date +"%h %d %Y %r"`" [INFO] Attempting to stop AEM with PID of $PID"
-  $STOP >> compact-startup.out 2>&1
+  PID=`cat ${PID_FILE}/conf/cq.pid`
+  echo `date +"%h %d %Y %r"`" [INFO] Attempting to stop AEM with PID of ${PID}"
+  $STOP
   while [ -n "`ps aux | grep 'java' | head -n -1 | grep ${PID}`" ]; do
     if [ ${COUNT} -ge ${TIMEOUT} ]; then
       echo `date +"%h %d %Y %r"`" [WARN] Instance failed to stop...aborting compaction."
       REASON="${ITYPE}Instance failed to stop after trying ${COUNT} times and waiting ${SLEEPTIME} each time."
-      #sendAlertEmail "${REASON}"
+      sendAlertEmail "${REASON}"
       exit 1
     fi
     echo `date +"%h %d %Y %r"`" [INFO] Instance not stopped completely, sleeping for $SLEEPTIME, check number ${COUNT}"
@@ -66,56 +67,56 @@ function stopAEM {
 }
 function compact {
 
-    if [ ! -f ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${1}.jar ];
+    if [ ! -f ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar ];
       then
        echo `date +"%h %d %Y %r"`" [INFO] Could not find valid oak run jar in the oak_run_jars directory."
        exit 1
     fi
-    if [ ! -d "$2" ];
+    if [ ! -d "${REPO_DIR}" ];
       then
         echo `date +"%h %d %Y %r"`" [INFO] Could not find directory for the repository."
         exit 1
       fi
-      if [ -z "$3" ];
+      if [ -z "${MODE}" ];
           then
             #they didn't supply a mode, run rm-unreferenced
             MODE='rm-unreferenced'
           else
-            MODE="$3"
+            MODE="${MODE}"
       fi
       ## check if they want to backup
-      if [ -n "$4" ];
+      if [ -n "${BACKUP}" ];
         then
-        if [ "$4" = 'true' ];
+        if [ "${BACKUP}" = 'true' ];
           then
-            su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-$VERSION.jar backup ${repoDir} ${repoDir}_bak" - ${COMPACTION_USER}
+            java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar backup "${REPO_DIR}" ${REPO_DIR}_bak
         fi
       fi
-      if [ -n "$pidFile" ];
+      if [ -n "${PID_FILE}" ];
         then
-          stopAEM $pidFile
+          stopAEM
       fi
-
-      VERSION=$1
       repoDir=$2
       echo "Switching user to ${COMPACTION_USER}"
-
+      echo "${javaOpts}"
+      echo "${MODE}"
+      echo "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar compact "${REPO_DIR}""
       # check for non-needed checkpoints
       echo -e `date +"%h %d %Y %r"`" [INFO] Using oak-run-${VERSION}.jar...\n"
       echo -e `date +"%h %d %Y %r"`" [INFO] Checking for checkpoints at $repoDir...\n"
-      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar checkpoints "${repoDir}"" - ${COMPACTION_USER}
+      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar checkpoints "${REPO_DIR}"" - ${COMPACTION_USER}
       # rm the checkpoints
       echo -e `date +"%h %d %Y %r"`" [INFO] ${MODE} checkpoints at $repoDir...\n"
-      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar checkpoints "${repoDir}" ${MODE}" - ${COMPACTION_USER}
+      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar checkpoints "${REPO_DIR}" ${MODE}" - ${COMPACTION_USER}
       #compact
       echo -e `date +"%h %d %Y %r"`" [INFO] Compacting segmentstore at $repoDir...\n"
-      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar compact "${repoDir}"" - ${COMPACTION_USER}
+      su -c "java ${javaOpts} -jar ${OAK_JARS_LOCATION}/oak_run_jars/oak-run-${VERSION}.jar compact "${REPO_DIR}"" - ${COMPACTION_USER}
       echo -e `date +"%h %d %Y %r"`" [INFO] Done!\n"
-      if [ -n "$pidFile" ];
+      if [ -n "${PID_FILE}" ];
         then
          echo "Starting AEM..."
-         $START >> compact-startup.out 2>&1
-         validateAEMStart $pidFile
+         ${START} >> compact.out
+         validateAEMStart ${PID_FILE}
       fi
       exit 0
 }
@@ -125,28 +126,28 @@ function init {
   while getopts ":d:H:v:m:b:p:j:i:" arg; do
     case "${arg}" in
       d)
-          repoDir=$OPTARG
+          REPO_DIR=$OPTARG
           ;;
       H)
           usage
           ;;
       v)
-          version=$OPTARG
+          VERSION=$OPTARG
           ;;
       m)
-          mode=$OPTARG
+          MODE=$OPTARG
           ;;
       b)
-          backup=$OPTARG
+          BACKUP=$OPTARG
           ;;
       p)
-          pidFile=$OPTARG
+          PID_FILE=$OPTARG
           ;;
       j)
           javaOpts="$OPTARG"
           ;;
       i)
-          instance=$OPTARG
+          INSTANCE=$OPTARG
           ;;
      *)
          usage
@@ -154,17 +155,17 @@ function init {
     esac
   done
   shift $((OPTIND-1))
-  if [ -z "${version}" ] || [ -z "${repoDir}" ]; then
+  if [ -z "${VERSION}" ] || [ -z "${REPO_DIR}" ]; then
     echo "It appears you did not specify all required arguments!"
     usage
   fi
-  echo -e `date +"%h %d %Y %r"`" [INFO] Compacting ${repoDir} with jar version ${version}..."
-  if [ "${instance}" == "publish" ]; then
+  echo -e `date +"%h %d %Y %r"`" [INFO] Compacting ${REPO_DIR} with jar version ${VERSION}..."
+  if [ "${INSTANCE}" == "publish" ]; then
     ITYPE='Publish '
     START='/etc/init.d/aem-publish start'
     STOP='/etc/init.d/aem-publish stop'
   fi
-  compact $version $repoDir $mode $backup "$javaOpts"
+  compact
 
  exit 0
 }
